a670ba1a26f8ffb26160a8837f8d1b9241397532
Yaroslav Ivanovskyi <yaroslav@outlook.sg>
2022-07-11 13:52:19 +0300

    spring

--- ch8/sq-ch8-ex6/src/main/java/com/example/controllers/ProductsController.java
+++ ch8/sq-ch8-ex6/src/main/java/com/example/controllers/ProductsController.java
@@ -37,3 +37,42 @@ public class ProductsController {
     return "products.html";
   }
 }
+
+/*
+
+@Controller
+public class ProductsController {
+
+    private final ProductService productService;
+
+    public ProductsController(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @GetMapping("/products")
+    public String viewProducts(Model model) {
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+
+    @PostMapping("/products")
+    public String addProduct(
+            @RequestParam String name,
+            @RequestParam double price,
+            Model model
+    ) {
+        Product p = new Product();
+        p.setName(name);
+        p.setPrice(price);
+        productService.addProduct(p);
+
+        var products = productService.findAll();
+        model.addAttribute("products", products);
+
+        return "products.html";
+    }
+}
+
+*/
